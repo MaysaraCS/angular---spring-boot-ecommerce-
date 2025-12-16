472c3729f5b3d2420da7026061452210e807c767
Maysara <maysaracs1001@gmail.com>
2025-12-16 16:44:58 +0300

    home added

---
+++ ecommerce-angular/src/app/home/main-carousel/main-carousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-carousel',
+  templateUrl: './main-carousel.component.html',
+  styleUrl: './main-carousel.component.css'
+})
+export class MainCarouselComponent {
+  carouselData:any
+
+}
